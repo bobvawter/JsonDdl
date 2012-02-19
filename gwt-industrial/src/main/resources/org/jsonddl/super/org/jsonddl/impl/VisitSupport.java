77a93d9a927971179792f302e69a5b93a1ff444f
Bob Vawter <bob@vawter.org>
2012-02-19 09:22:41 -0700

    Misc cleanups.

--- gwt-industrial/src/main/resources/org/jsonddl/super/org/jsonddl/impl/VisitSupport.java
+++ gwt-industrial/src/main/resources/org/jsonddl/super/org/jsonddl/impl/VisitSupport.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2012 Robert W. Vawter III <bob@vawter.org>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.jsonddl.impl;
 
 import java.io.UnsupportedEncodingException;
@@ -8,18 +21,12 @@ import org.jsonddl.JsonDdlVisitor.Context;
 import org.jsonddl.impl.ContextImpl.ObjectContext;
 
 public class VisitSupport {
-  private static final String MESSAGE = "Cannot use ad-hoc visitors with GWT mode. " +
-      "Extend the package's visitor base class instead";
-
   public static <J extends JsonDdlObject<J>> void endVisit(JsonDdlVisitor visitor, J obj,
-      Context<J> ctx) {
-    // throw new UnsupportedOperationException(MESSAGE);
-  }
+      Context<J> ctx) {}
 
   public static <J extends JsonDdlObject<J>> boolean visit(JsonDdlVisitor visitor, J obj,
       Context<J> ctx) {
     return true;
-    // throw new UnsupportedOperationException(MESSAGE);
   }
 
   @SuppressWarnings("unchecked")
